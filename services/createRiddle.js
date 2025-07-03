843f069fa29281d2c526a625bafe852da21bc96f
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-03 16:43:05 +0300

    added show riddles

---
+++ services/createRiddle.js
@@ -0,0 +1,36 @@
+import readline from 'readline-sync';
+import {Riddle} from '../classes/Riddle.js'
+import fs from 'node:fs';
+
+
+function createRiddle(){
+    const riddleName = readline.question("What is the name of the riddle you want to add?")
+    const difficulty = readline.question("What is the difficuly level?(easy, medium , hard)")
+    const describe = readline.question("Describe the riddle.");
+    const answer = readline.question("What is the answer to the riddle?")
+
+
+    let yourRiddle = new Riddle(difficulty,1,riddleName,describe,answer)
+}
+
+function sendRiddleToDB(riddle){
+    fs.readFile("../riddlesDB/riddleList.txt","utf8",(err,data) => {
+        if(err){
+            console.log("Error reading file:",err)
+            return
+        }
+        const riddleArray = JSON.parse(data);
+        riddleArray.push(riddle);
+
+        fs.writeFile("riddleList.txt",JSON.stringify(riddleArray,null,2),"utf-8",(err) => {
+            if(err){
+                console.error("error writing file")
+            }
+            else{
+                console.log("object added successfully!")
+            }
+        })
+       
+    })
+}
+
