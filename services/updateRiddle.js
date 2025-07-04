104aa8916da3407cbc3b084c988e458aafc7bb46
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-04 14:39:09 +0300

    added update feature

--- services/updateRiddle.js
+++ services/updateRiddle.js
@@ -1,13 +1,12 @@
 import readline from 'readline-sync';
 import fs, { read } from 'node:fs';
 
-function riddlePartToChange(riddleId){
+
+
+function changeRiddle(riddleId){
     const choice = readline.question(`Which part of the riddle do you want to change?(name,difficulty,question,answer)`)
     const newVersion = readline.question("What is the version after the change?")
-}
 
-
-function changeRiddle(){
     fs.readFile("../riddlesDB/riddleList.txt","utf8",(err,data) => {
         if(err){
             console.log("error reading file");
@@ -15,8 +14,22 @@ function changeRiddle(){
         }
         const riddleArray = JSON.parse(data)
 
+        for(const riddle of riddleArray){
+            if(riddle.id === riddleId){
+                riddle[choice] = newVersion
+                break
+            }
+        }
+        const jsonString = JSON.stringify(riddleArray,null,2)
+        fs.writeFile("../riddlesDB/riddleList.txt",jsonString,(err) => {
+            if(err){
+                console.log("error writing to file")
+            }
+            else{
+                console.log("file saved successfully!")
+            }
+        })
 
     })
 }
-
 
