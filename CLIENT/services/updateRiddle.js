5eb1092116057e097c9eab7509241f1c9bfabbf0
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-10 15:32:00 +0300

    test

---
+++ CLIENT/services/updateRiddle.js
@@ -0,0 +1,26 @@
+import readline from 'readline-sync';
+import fs, { read } from 'node:fs';
+import { error } from 'node:console';
+
+
+
+function changeRiddle(riddleId){
+    const choice = readline.question(`Which part of the riddle do you want to change?(name,difficulty,question,answer)`)
+    const newVersion = readline.question("What is the version after the change?")
+
+     
+   
+}
+
+
+
+
+
+
+
+ for(const riddle of riddleArray){             
+            if(riddle.id === riddleId){
+                riddle[choice] = newVersion
+                break
+            }
+        }
