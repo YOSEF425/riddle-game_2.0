c64b87a46092f406e0b62ea1bf2c3a1cbaf157d4
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-22 17:36:19 +0300

    added

--- CLIENT/services/createRiddle.js
+++ CLIENT/services/createRiddle.js
@@ -1,18 +1,19 @@
 import readline from 'readline-sync';
-import {Riddle} from '../classes/Riddle.js'
+import { Riddle } from '../classes/Riddle.js'
 import fs from 'node:fs';
+import { addRiddle } from '../api/riddles.js';
 
 
-export async function createRiddle(){     // create riddle "object" , get all properties from user
+export async function createRiddle() {     // create riddle "object" , get all properties from user
     const riddleName = readline.question("What is the name of the riddle you want to add?")
     const difficulty = readline.question("What is the difficuly level?(easy, medium , hard)")
     const describe = readline.question("Describe the riddle!");
     const answer = readline.question("What is the answer to the riddle?")
 
 
-    let myRiddle = new Riddle(difficulty,riddleName,describe,answer);
+    let myRiddle = new Riddle(difficulty, riddleName, describe, answer);
 
-  
+    
         const response = await fetch('http://localhost:5000/api/riddles',{
          method: 'POST',
          headers: {
@@ -23,10 +24,10 @@ export async function createRiddle(){     // create riddle "object" , get all pr
         })
         const result = await response.text()
         console.log(result)
-        
-    }
 
-     
+}
+
+
 
 
 
