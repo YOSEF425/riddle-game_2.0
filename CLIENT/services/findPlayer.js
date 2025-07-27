ffa7077d62db6ccc0da4c7dbd31859852991ae56
Yosef Steinberg <steinbergyosef@gmail.com>
2025-07-27 19:37:38 +0300

    added

--- CLIENT/services/findPlayer.js
+++ CLIENT/services/findPlayer.js
@@ -1,4 +1,4 @@
-export async function findPlayer(name){
+export async function findPlayer(name){     /// Just to check if player is in system i'm using his unique name. Otherwise using token.
    const response =  fetch(`http://localhost:5000/findPlayerByName/${name}`)
    const text = await response.text
    console.log(text)
